7e523d9027700d961d18f82d0531cfc62962ab02
Jacob Voyles <jacobvoyles@Jacobs-MacBook-Pro.local>
2020-04-23 15:15:51 -0500

    Fixed the adding feature and added a removal and check feature.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
+var idNum = 0;
 function addElement(){
   var ul = document.getElementById("ulDiv");
-  var li = document.createElement("li");
   var item = document.getElementById('newInput');
+  var li = document.createElement("li");
+  var button = document.createElement("button");
+  var checkmark = document.createElement("button");
+  checkmark.setAttribute("id",idNum.toString());
+  checkmark.setAttribute("class", "fa fa-check");
+  checkmark.setAttribute("onclick", "checkElement(this.id)");
+  button.setAttribute("id",idNum.toString());
+  button.setAttribute("class","fa fa-trash");
+  button.setAttribute("onclick", "removeElement(this.id)");
+  li.setAttribute("id",idNum.toString());
   li.appendChild(document.createTextNode(item.value));
+  li.appendChild(button);
+  li.appendChild(checkmark);
   ul.appendChild(li);
+  idNum++;
 }
 
-document.body.onkeyup = function(e) {
-  if (e.keyCode == 13) {
-    newItem();
-  }
-};
-
-function removeItem(e) {
-  e.target.parentElement.removeChild(e.target);
+function removeElement(id){
+  element = document.getElementById(id);
+  element.parentNode.removeChild(element);
 }
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+function checkElement(id){
+  const element = document.getElementById(id);
+
+  if(element.classList.contains("checked") == false){
+    element.classList.add("checked");
+  }else{
+    element.classList.remove("checked");
   }
-}, false);
+}
