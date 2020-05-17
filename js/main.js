b0a7dd7d2efe858d75f742da1a050e49abbde63a
jtvkw2 <jtvkw2@gmail.com>
2020-05-16 21:35:14 -0500

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var idNum = 0;
-function addElement(){
+var idNum = 0; // Variable for increasing To-Do Elements on Page
+
+function addElement(){ // Function that Adds Element, triggered onclick
   var ul = document.getElementById("ulDiv");
   var item = document.getElementById('newInput');
   var li = document.createElement("li");
@@ -19,15 +20,14 @@ function addElement(){
   ul.appendChild(li);
   item.value = "";
   idNum++;
-
 }
 
-function removeElement(id){
+function removeElement(id){ //Removes Task from list, takes one variable that is the id of element
   element = document.getElementById(id);
   element.parentNode.removeChild(element);
 }
 
-function checkElement(id){
+function checkElement(id){ //Marks the task completed if not already so, triggered onclick
   const element = document.getElementById(id);
 
   if(element.classList.contains("checked") == false){
